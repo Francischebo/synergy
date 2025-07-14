4d9b010f8cf39f4677dffb47549722c330a924c2
Francis Chebo Ividah <86152359+Francischebo@users.noreply.github.com>
2025-07-14 14:59:46 +0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect("mongodb+srv://Francischebo:<qtgN3ttRDgBec89S>@ysn-website.wcjg1tt.mongodb.net/?retryWrites=true&w=majority&appName=ysn-website");
         console.log('✅ MongoDB connected successfully');
     } catch (err) {
         console.error('❌ MongoDB connection error:', err.message);
